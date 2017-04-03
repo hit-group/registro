0a67e9fc7308b7f5a4664d5329b5cc3406f4a826
Aleks26 <aleks26@live.it>
2017-04-03 16:42:13 +0200

    Add files via upload

--- voti.php
+++ voti.php
@@ -6,25 +6,73 @@
 
   <body>
     <div class="form-group">
-      <label>Classe</label>
-      <select class="form-control select2" style="width: 100%;">
-        <option>3 AFM S</option>
-        <option>4 AFM S</option>
-        <option>5 AFM S</option>
-        <option>3 CAT S</option>
-        <option>4 CAT S</option>
-        <option>5 CAT S</option>
-      </select>
+      <label>VOTI</label>
+      <?php
+
+        $con=new mysqli("localhost","root","","registro_db");
+
+        $result = $con->query("SELECT * FROM classi");
+
+        if(!$con){
+
+            echo "Connessione al database fallita";
+
+        }
+
+
+
+
+      echo '
+	<div class="box box-primary">
+	  <div class="box-header with-border">
+	    <h3 class="box-title">Inserimento voti</h3>
+	  </div>
+	  <!-- /.box-header -->
+	  <!-- form start -->
+
+
+	  <form id="usrregform" role="form" method="post" action="/include/forms/rmclass.php">
+	    <div class="box-body">
+	      <div class="form-group">
+	        <label>Classe</label>
+	        <select id="roleselect"  name="classe" form="usrregform" class="form-control select2" style="width: 20%;">
+						<option value="" disabled selected>Scegli una classe</option>
+	          ';
+
+							if ($result->num_rows > 0) {
+									// output data of each row
+									while($row = $result->fetch_assoc()) {
+
+										$annoB = intval($row["anno"]); //2017
+										$annoA = $annoB-1; 						 //2016
+										echo '<option value='.$row["id"].'>'.$row["nome"].' '.$annoA.'-'.$annoB.'</option>';
+									}
+							 } else {
+
+							 }
+
+
+	        echo '</select>
+	      </div>
+
+
+	    </div>
+	    <!-- /.box-body -->
+
+	    <div class="box-footer">
+	    </div>
+	  </form>
+	</div>
+	';?>
 
 
       <div class="form-group">
         <label>Voto</label>
-        <select class="form-control select2" style="width: 100%;">
+        <select class="form-control select2" style="width: 5%;">
+          <option value="" disabled selected>Seleziona il voto</option>
           <option>10</option>
           <option>9.5</option>
           <option>9</option>
-          <option>8.5</option>
-          <option>8</option>
           <option>7.5</option>
           <option>7</option>
           <option>6.5</option>
@@ -37,23 +85,16 @@
 
         <div class="form-group">
           <label>Tipo prova</label>
-          <select class="form-control select2" style="width: 100%;">
+          <select class="form-control select2" style="width: 10%;">
+            <option value="" disabled selected>Seleziona il tipo di prova</option>
             <option>Prova scritta</option>
             <option>Prova pratica</option>
             <option>Prova orale</option>
     </div>
 
     <input type="submit" value="INVIA">
-
     <?php
 
-      $con=new mysqli("localhost","root","","registro_db");
-
-      if(!$con){
-
-          echo "Connessione al database fallita";
-
-      }
 
       $result = $con-> query("SELECT cognome from utenti");
 
@@ -64,34 +105,9 @@
 
               echo "".$array['cognome']."<br>";
 
-
-
-
         }
     }
 
-
-
-    $result = $con-> query("SELECT classe from utenti");
-
-    //query("SELECT u.nome,u.cognome,u.classe,c.id,c.nome as nomeClasse,u.username,u.email,u.ruolo FROM utenti as u, classi as c WHERE u.classe = c.id");
-    if($result->num_rows>0){
-
-      while($array = $result-> fetch_array(MYSQLI_ASSOC)){
-
-            echo "".$array['classe']."<br>";
-
-
-
-
-      }
-  }
-
-
-
-
-
     ?>
-
   </body>
 </html>
